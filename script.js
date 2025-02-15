aacd681b81f47a67864b52dfb91a24891fdeb2e2
Ankit Pramanik <ankitpramanik849@gmail.com>
2025-02-15 14:46:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-// Hamburger Menu Toggle
-function toggleMenu() {
-  const navLinks = document.getElementById('nav-links');
-  navLinks.classList.toggle('active');
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform  = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform  = "translateY(-500px)"
 }
 
-// Scroll Animations
-const animateElements = document.querySelectorAll('.animate');
+// for Typewriter effect
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+const texts = [
+    "DEVELOPER",
+    "YOUTUBER",
+    "DESIGNER"
+]
+
+let speed = 100;
+
+const textElements = document.querySelector(".typewriter-text")
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter() {
+    if(charcterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+        charcterIndex++;
+        setTimeout(typeWriter, speed); 
     }
-  });
-});
+    else{
+        setTimeout(eraseText, 1000)
+    }
+}
 
-animateElements.forEach((el) => observer.observe(el));
+function eraseText() {
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0,-1)
+        setTimeout(eraseText, 50)
+    }
+    else{
+        textIndex = (textIndex + 1) % texts.length;
+        charcterIndex = 0;
+        setTimeout(typeWriter,500)
+    }
+}
+
+window.onload = typeWriter;
